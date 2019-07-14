38b8f7c5d32af05f5450cb926a783ba195ed881c
Yogen Rai <yogen.rai.992@gmail.com>
2019-07-14 16:42:47 -0500

    Added more example for update and read

--- java-core/src/main/java/com/vastika/training/java/jdbc/cms/DeriverProgram.java
+++ java-core/src/main/java/com/vastika/training/java/jdbc/cms/DeriverProgram.java
@@ -1,13 +1,61 @@
 package com.vastika.training.java.jdbc.cms;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class DeriverProgram {
     public static void main(String[] args) {
-        StudentRepository repository = new StudentRepository();
+        String input = "yes";
+        while (!"no".equals(input)) {
+            System.out.println("Please select: ");
+            System.out.println("1. To list all student");
+            System.out.println("2. To print student details");
+            System.out.println("3. To update student record");
 
-        List<Student> students = repository.getAllStudents();
+            // add functionality to deleete
 
-        System.out.println(students);
+            System.out.println("---------------------------");
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+
+            StudentRepository repository = new StudentRepository();
+
+            if (choice == 1) {
+                List<Student> students = repository.getAllStudents();
+
+                System.out.println(students);
+            } else if (choice == 2) {
+
+                System.out.print("Enter student id: ");
+                int studentId = scanner.nextInt();
+                Student studentId2 = repository.findStudentById(studentId);
+                System.out.println(studentId2);
+            } else if (choice == 3) {
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Enter student id: ");
+                int id = Integer.valueOf(sc.next());
+                System.out.print("Enter student first name: ");
+                String firstName = sc.next();
+                System.out.print("Enter student last name: ");
+                String lastName = sc.next();
+                System.out.print("Enter student gpa: ");
+                double gpa = Double.valueOf(sc.next());
+                Student toUpdate = new Student(id, firstName, lastName, gpa);
+
+                boolean updated = repository.updateStudent(toUpdate);
+
+                if (updated) {
+                    System.out.println("Success!");
+                } else {
+                    System.out.println("Failed!");
+                }
+            }
+
+            System.out.println("---------------------------");
+
+            System.out.println("Do you want to search again? (yes/no)");
+            Scanner inputScanner = new Scanner(System.in);
+            input = inputScanner.next();
+        }
     }
 }
--- java-core/src/main/java/com/vastika/training/java/jdbc/cms/StudentRepository.java
+++ java-core/src/main/java/com/vastika/training/java/jdbc/cms/StudentRepository.java
@@ -33,6 +33,46 @@ public class StudentRepository {
         }
 
         return students;
+    }
 
+    public Student findStudentById(int userId) {
+        Connection connection = DbConnector.getConnection();
+
+        try {
+            PreparedStatement ps = connection.prepareStatement("select * from student where id = " + userId);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String firstName = rs.getString("firstName");
+                String lastName = rs.getString("lastName");
+                double gpa = rs.getDouble("gpa");
+
+                return new Student(id, firstName, lastName, gpa);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public boolean updateStudent(Student student) {
+        Connection connection = DbConnector.getConnection();
+
+        try {
+            String sql = "update student set firstName='" + student.getFirstName()
+                    + "', lastName='" + student.getLastName() + "', gpa=" + student.getGpa() + " where id=" + student.getId();
+
+            PreparedStatement ps = connection.prepareStatement(sql);
+
+            int rs = ps.executeUpdate();
+
+            return rs > 0 ? true : false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
