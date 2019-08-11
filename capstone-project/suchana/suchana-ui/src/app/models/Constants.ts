e548dc641a1dca55c949bf7843e74f3336bcb4b8
Yogen Rai <yogen.rai.992@gmail.com>
2019-08-11 15:47:24 -0500

    Added service for suchana api

--- capstone-project/suchana/suchana-ui/src/app/models/Constants.ts
+++ capstone-project/suchana/suchana-ui/src/app/models/Constants.ts
@@ -1,9 +1,9 @@
 export const Constants = {
   dev: {
-    API_BASE_URL: 'http://dev.suchana.com'
+    API_BASE_URL: '18.204.205.35:8080'
   },
   prod: {
     API_BASE_URL: 'https://suchana.com'
   },
-  API_BASE_URL: "http://localhost:8080"
+  API_BASE_URL: "http://18.204.205.35:8080"
 };
